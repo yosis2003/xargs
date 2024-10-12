d38c0427b0e00c4aef7c475fe8f5f55ceddb9e58
yosis2003 <yousif_fadi@hotmail.com>
2024-10-11 17:35:48 -0700

    rewrote script + lmao some notes

--- testerscript.sh
+++ testerscript.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+testCommand = $(echo mrbean.txt guyfawkes.txt mugabe.txt brownie.txt linus.txt | java MyXargs.java -n 2 ls)
+realCommand = $(echo mrbean.txt guyfawkes.txt mugabe.txt brownie.txt linus.txt | xargs -n 2 ls)
 
+if ["$realCommand" == "$testCommand"]; then
+    echo "passed -n 2 case";
+else
+    echo "failed -n 2";
+fi
 
-echo mrbean.txt guyfawkes.txt mugabe.txt brownie.txt linus.txt | java MyXargs.java -n 2 ls
